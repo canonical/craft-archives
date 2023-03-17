2038e7e78e7dcf43cff3176b0fdc3a1c2cf9af9d
Alex Lowe <alex.lowe@canonical.com>
2023-03-17 14:59:56 -0400

    copyright update from PR comments
    Signed-off-by: Alex Lowe <alex.lowe@canonical.com>

--- tests/unit/repo/test_apt_preferences_manager.py
+++ tests/unit/repo/test_apt_preferences_manager.py
@@ -1,9 +1,9 @@
 # -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
 #
-# Copyright 2015-2023 Canonical Ltd.
+# Copyright 2023 Canonical Ltd.
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 3 as
+# it under the terms of the GNU Lesser General Public License version 3 as
 # published by the Free Software Foundation.
 #
 # This program is distributed in the hope that it will be useful,
@@ -11,7 +11,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 """Tests for apt_preferencs_manager"""
