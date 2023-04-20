c9bd220c613058b96155278dc8cb41db25058a1d
Tiago Nobrega <tiago.nobrega@canonical.com>
2023-04-20 18:16:34 -0300

    repo: unify package repository representations (#57)
    This commit unifies the repo models from `package_repositories` and
    `projects`. Remarks:
    
    - The Apt, AptDeb, AptPPA and AptUCA classes are removed completely
      from the `projects` module. The verification that they performed is
      now done by their "duals" in `package_repositories`.
    - `projects.validate_repository()` is preserved as it's part of the
      "official" public API. Its implementation is changed to just call
      PackageRepository.unmarshal(), with (hopefully) no change in
      semantics.
    - The `PackageRepositoryApt.name` property is removed; as far as I can
      tell only test code used it and it's not documented (it looks like
      it's not meant to be used by client code), so might as well remove it
      while we're cleaning things up.
    - The majority of the model validation now generates Pydantic errors
      instead of the custom `errors.PackageRepositoryValidationError`. This
      is consistent with what the applications do but necessitates a small
      loss in "error clarity" because we remove the "resolution" and
      "details" fields. Hopefully the messages are still sufficiently
      clear.
    - There were many duplicated tests between `test_package_repository.py`
      and `test_projects.py`, because each set performed the same validation
      tests in the two model classes "types". Now the majority of the tests
      are in `test_package_repository.py`.
    - A minor point; the "priority" field is read as either an integer or
      one of the valid string values, but stored internally always as an
      integer (or None).

--- tests/unit/repo/test_apt_sources_manager.py
+++ tests/unit/repo/test_apt_sources_manager.py
@@ -28,6 +28,8 @@ from craft_archives.repo.package_repository import (
     PackageRepositoryAptUCA,
 )
 
+# pyright: reportGeneralTypeIssues=false
+
 
 @pytest.fixture(autouse=True)
 def mock_apt_ppa_get_signing_key(mocker):
@@ -81,6 +83,7 @@ def apt_sources_mgr(tmp_path):
     [
         (
             PackageRepositoryApt(
+                type="apt",
                 architectures=["amd64", "arm64"],
                 components=["test-component"],
                 formats=["deb", "deb-src"],
@@ -102,62 +105,26 @@ def apt_sources_mgr(tmp_path):
         ),
         (
             PackageRepositoryApt(
+                type="apt",
                 architectures=["amd64", "arm64"],
-                components=["test-component"],
+                formats=["deb", "deb-src"],
+                path="dir/subdir",
                 key_id="A" * 40,
-                name="NO-FORMAT",
-                suites=["test-suite1", "test-suite2"],
                 url="http://test.url/ubuntu",
             ),
-            "craft-NO-FORMAT.sources",
+            "craft-http_test_url_ubuntu.sources",
             dedent(
                 """\
-                Types: deb
-                URIs: http://test.url/ubuntu
-                Suites: test-suite1 test-suite2
-                Components: test-component
-                Architectures: amd64 arm64
-                Signed-By: {keyring_path}
-                """
+                    Types: deb deb-src
+                    URIs: http://test.url/ubuntu
+                    Suites: dir/subdir/
+                    Architectures: amd64 arm64
+                    Signed-By: {keyring_path}
+                    """
             ),
         ),
         (
-            PackageRepositoryApt(
-                key_id="A" * 40,
-                name="WITH-PATH",
-                path="some-path",
-                url="http://test.url/ubuntu",
-            ),
-            "craft-WITH-PATH.sources",
-            dedent(
-                """\
-                Types: deb
-                URIs: http://test.url/ubuntu
-                Suites: some-path/
-                Architectures: FAKE-HOST-ARCH
-                Signed-By: {keyring_path}
-                """
-            ),
-        ),
-        (
-            PackageRepositoryApt(
-                key_id="A" * 40,
-                name="IMPLIED-PATH",
-                url="http://test.url/ubuntu",
-            ),
-            "craft-IMPLIED-PATH.sources",
-            dedent(
-                """\
-                Types: deb
-                URIs: http://test.url/ubuntu
-                Suites: /
-                Architectures: FAKE-HOST-ARCH
-                Signed-By: {keyring_path}
-                """
-            ),
-        ),
-        (
-            PackageRepositoryAptPPA(ppa="test/ppa"),
+            PackageRepositoryAptPPA(type="apt", ppa="test/ppa"),
             "craft-ppa-test_ppa.sources",
             dedent(
                 """\
@@ -171,7 +138,7 @@ def apt_sources_mgr(tmp_path):
             ),
         ),
         (
-            PackageRepositoryAptUCA(cloud="fake-cloud"),
+            PackageRepositoryAptUCA(type="apt", cloud="fake-cloud"),
             "craft-cloud-fake-cloud.sources",
             dedent(
                 """\
@@ -227,7 +194,7 @@ def test_install(package_repo, name, content_template, apt_sources_mgr, mocker):
 
 
 def test_install_ppa_invalid(apt_sources_mgr):
-    repo = PackageRepositoryAptPPA(ppa="ppa-missing-slash")
+    repo = PackageRepositoryAptPPA(type="apt", ppa="ppa-missing-slash")
 
     with pytest.raises(errors.AptPPAInstallError) as raised:
         apt_sources_mgr.install_package_repository_sources(package_repo=repo)
@@ -242,7 +209,7 @@ def test_install_ppa_invalid(apt_sources_mgr):
     side_effect=urllib.error.HTTPError("", http.HTTPStatus.NOT_FOUND, "", {}, None),  # type: ignore
 )
 def test_install_uca_invalid(urllib, apt_sources_mgr):
-    repo = PackageRepositoryAptUCA(cloud="FAKE-CLOUD")
+    repo = PackageRepositoryAptUCA(type="apt", cloud="FAKE-CLOUD")
     with pytest.raises(errors.AptUCAInstallError) as raised:
         apt_sources_mgr.install_package_repository_sources(package_repo=repo)
 
@@ -258,43 +225,12 @@ class UnvalidatedAptRepo(PackageRepositoryApt):
         pass
 
 
-@pytest.mark.parametrize(
-    "repo,error_cls,error_match",
-    [
-        pytest.param(
-            PackageRepositoryApt(
-                architectures=["amd64"],
-                url="https://example.com",
-                key_id="A" * 40,
-            ),
-            errors.AptGPGKeyringError,
-            "",
-            id="keyring error",
-        ),
-        pytest.param(
-            UnvalidatedAptRepo(
-                architectures=["amd64"],
-                url="https://example.com",
-                suites=["honeymoon", "presidential"],
-                key_id="A" * 40,
-            ),
-            RuntimeError,
-            "no components with suite",
-            id="no components with suite",
-        ),
-        pytest.param(
-            UnvalidatedAptRepo(
-                architectures=["amd64"],
-                url="https://example.com",
-                components=["handlebar"],
-                key_id="A" * 40,
-            ),
-            RuntimeError,
-            "no suites or path",
-            id="no suites or path",
-        ),
-    ],
-)
-def test_install_apt_errors(repo, error_cls, error_match, apt_sources_mgr):
-    with pytest.raises(error_cls, match=error_match):
+def test_install_apt_errors(apt_sources_mgr):
+    repo = PackageRepositoryApt(
+        type="apt",
+        architectures=["amd64"],
+        url="https://example.com",
+        key_id="A" * 40,
+    )
+    with pytest.raises(errors.AptGPGKeyringError):
         apt_sources_mgr._install_sources_apt(package_repo=repo)
