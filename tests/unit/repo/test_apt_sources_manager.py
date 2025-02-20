63d847356a513fc6d857cc49788cf304f5f3bacd
Callahan Kovacs <callahankovacs@gmail.com>
2025-02-20 08:14:26 -0600

    feat: support foreign architectures
    Signed-off-by: Callahan Kovacs <callahankovacs@gmail.com>

--- tests/unit/repo/test_apt_sources_manager.py
+++ tests/unit/repo/test_apt_sources_manager.py
@@ -33,6 +33,8 @@ from craft_archives.repo.apt_sources_manager import (
     AptSourcesManager,
     _add_architecture,
     _get_suites,
+    _is_deb822_default,
+    _update_sources_file,
 )
 from craft_archives.repo.package_repository import (
     PackageRepositoryApt,
@@ -89,6 +91,14 @@ def apt_sources_mgr(tmp_path):
         sources_list_d=sources_list_d,
         keyrings_dir=keyrings_dir,
         signed_by_root=tmp_path,
+    )
+
+
+@pytest.fixture
+def mock_is_deb822_default(request, mocker):
+    return mocker.patch(
+        "craft_archives.repo.apt_sources_manager._is_deb822_default",
+        return_value=getattr(request, "param", False),
     )
 
 
@@ -291,7 +301,11 @@ def test_install(
 
     if isinstance(package_repo, PackageRepositoryApt) and package_repo.architectures:
         assert add_architecture_mock.mock_calls == [
-            call(package_repo.architectures, root=expected_root)
+            call(
+                architectures=package_repo.architectures,
+                root=expected_root,
+                sources_dir=apt_sources_mgr._sources_list_d,
+            )
         ]
         assert get_architecture_mock.called
 
@@ -365,18 +379,26 @@ def test_preferences_path_for_root():
 
 
 @pytest.mark.parametrize(
-    ("host_arch, repo_arch"),
+    ("host_arch", "repo_arch"),
     [
         (b"amd64\n", "i386"),
+        (b"i386\n", "amd64"),
         (b"arm64\n", "armhf"),
+        (b"armhf\n", "arm64"),
     ],
 )
-def test_add_architecture_compatible(mocker, host_arch, repo_arch):
-    """Test calling _add_architecture() with compatible pairs of (host, repo)."""
+@pytest.mark.usefixtures("mock_is_deb822_default")
+def test_add_architecture_compatible_not_deb822(mocker, host_arch, repo_arch):
+    """Add compatible architectures even if the default sources aren't deb822."""
+    update_sources_file_mock = mocker.patch(
+        "craft_archives.repo.apt_sources_manager._update_sources_file"
+    )
     check_output_mock = mocker.patch("subprocess.check_output", return_value=host_arch)
     run_mock = mocker.patch("subprocess.run")
 
-    _add_architecture([repo_arch], root=Path("/"))
+    _add_architecture(
+        [repo_arch], root=Path("/"), sources_dir=_DEFAULT_SOURCES_DIRECTORY
+    )
 
     check_output_mock.assert_called_once_with(["dpkg", "--print-architecture"])
     assert run_mock.mock_calls == [
@@ -385,24 +407,188 @@ def test_add_architecture_compatible(mocker, host_arch, repo_arch):
             check=True,
         ),
     ]
+    update_sources_file_mock.assert_not_called()
 
 
 @pytest.mark.parametrize(
-    ("host_arch, repo_arch"),
+    ("host_arch", "repo_arch"),
     [
         (b"amd64\n", "arm64"),
         (b"arm64\n", "i386"),
     ],
 )
-def test_add_architecture_incompatible(mocker, host_arch, repo_arch):
-    """Test calling _add_architecture() with incompatible pairs of (host, repo)."""
+@pytest.mark.usefixtures("mock_is_deb822_default")
+def test_add_architecture_incompatible_not_deb822(mocker, host_arch, repo_arch):
+    """Don't add incompatible architectures if the default sources aren't deb822."""
+    update_sources_file_mock = mocker.patch(
+        "craft_archives.repo.apt_sources_manager._update_sources_file"
+    )
     check_output_mock = mocker.patch("subprocess.check_output", return_value=host_arch)
     run_mock = mocker.patch("subprocess.run")
 
-    _add_architecture([repo_arch], root=Path("/"))
+    _add_architecture(
+        [repo_arch], root=Path("/"), sources_dir=_DEFAULT_SOURCES_DIRECTORY
+    )
 
     check_output_mock.assert_called_once_with(["dpkg", "--print-architecture"])
     assert not run_mock.called
+    update_sources_file_mock.assert_not_called()
+
+
+@pytest.mark.parametrize(
+    ("host_arch", "compatible_archs", "repo_arch"),
+    [
+        pytest.param(b"amd64\n", ["amd64", "i386"], "arm64", id="compatible-arch-pair"),
+        pytest.param(b"riscv64\n", "riscv64", "arm64", id="lone-arch"),
+    ],
+)
+@pytest.mark.parametrize("mock_is_deb822_default", [True], indirect=True)
+def test_add_architecture_deb822(
+    mocker, host_arch, compatible_archs, repo_arch, mock_is_deb822_default
+):
+    """Update default sources and add archs if the default sources are deb822."""
+    update_sources_file_mock = mocker.patch(
+        "craft_archives.repo.apt_sources_manager._update_sources_file"
+    )
+    check_output_mock = mocker.patch("subprocess.check_output", return_value=host_arch)
+    run_mock = mocker.patch("subprocess.run")
+
+    _add_architecture(
+        [repo_arch], root=Path("/"), sources_dir=_DEFAULT_SOURCES_DIRECTORY
+    )
+
+    check_output_mock.assert_called_once_with(["dpkg", "--print-architecture"])
+    assert run_mock.mock_calls == [
+        call(
+            ["dpkg", "--root", "/", "--add-architecture", repo_arch],
+            check=True,
+        ),
+    ]
+    update_sources_file_mock.assert_called_once_with(
+        sources_file=_DEFAULT_SOURCES_DIRECTORY / "ubuntu.sources",
+        field="Architectures",
+        values=compatible_archs,
+    )
+
+
+@pytest.mark.parametrize(
+    ("archs_entry", "archs_to_update", "expected_archs_entry"),
+    [
+        pytest.param("", ["riscv64"], "Architectures: riscv64\n", id="add-one"),
+        pytest.param(
+            "", ["amd64", "i386"], "Architectures: amd64 i386\n", id="add-multiple"
+        ),
+        pytest.param(
+            "Architectures: riscv64",
+            ["riscv64"],
+            "Architectures: riscv64\n",
+            id="update-no-op",
+        ),
+        pytest.param(
+            "Architectures: amd64",
+            ["riscv64"],
+            "Architectures: riscv64\n",
+            id="update-one",
+        ),
+        pytest.param(
+            "Architectures: amd64",
+            ["amd64", "i386"],
+            "Architectures: amd64 i386\n",
+            id="update-multiple",
+        ),
+        pytest.param("Architectures: amd64 i386", "", "", id="delete-empty-str"),
+        pytest.param("Architectures: amd64 i386", [], "", id="delete-empty-list"),
+    ],
+)
+def test_update_sources(tmp_path, archs_entry, archs_to_update, expected_archs_entry):
+    """Update values in a deb822 sources files."""
+    sources_file = (
+        tmp_path / _DEFAULT_SOURCES_DIRECTORY.relative_to("/") / "ubuntu.sources"
+    )
+    sources = textwrap.dedent(
+        """
+        Types: deb
+        URIs: http://archive.ubuntu.com/ubuntu/
+        Suites: noble noble-updates noble-backports
+        Components: main universe restricted multiverse
+        Signed-By: /usr/share/keyrings/FC42E99D.gpg
+        """
+    )
+    sources_file.parent.mkdir(parents=True)
+    sources_file.write_text(sources + archs_entry)
+
+    _update_sources_file(
+        sources_file=sources_file,
+        field="Architectures",
+        values=archs_to_update,
+    )
+
+    assert sources_file.read_text() == sources.lstrip() + expected_archs_entry + "\n"
+
+
+def update_sources_does_not_exist(tmp_path):
+    """No-op if the sources file does not exist."""
+    sources_file = (
+        tmp_path / _DEFAULT_SOURCES_DIRECTORY.relative_to("/") / "ubuntu.sources"
+    )
+    sources_file.parent.mkdir(parents=True)
+
+    _update_sources_file(
+        sources_file=sources_file,
+        field="Architectures",
+        values="riscv64",
+    )
+
+    assert not sources_file.exists()
+
+
+def test_update_sources_wrong_format(tmp_path):
+    """Don't touch non-deb822 files."""
+    sources_file = (
+        tmp_path / _DEFAULT_SOURCES_DIRECTORY.relative_to("/") / "ubuntu.sources"
+    )
+    sources_file.parent.mkdir(parents=True)
+    sources_file.write_text("not a sources file\n")
+
+    _update_sources_file(
+        sources_file=sources_file,
+        field="Architectures",
+        values="riscv64",
+    )
+
+    assert sources_file.read_text() == "not a sources file\n"
+
+
+def test_is_deb822_default(tmp_path):
+    _is_deb822_default.cache_clear()
+    sources_file = tmp_path / "ubuntu.sources"
+    sources_file.write_text(
+        textwrap.dedent(
+            """
+            Types: deb
+            URIs: http://archive.ubuntu.com/ubuntu/
+            Suites: noble noble-updates noble-backports
+            Components: main universe restricted multiverse
+            Signed-By: /usr/share/keyrings/FC42E99D.gpg
+            """
+        )
+    )
+
+    assert _is_deb822_default(tmp_path)
+
+
+def test_is_deb822_default_no_file(tmp_path):
+    _is_deb822_default.cache_clear()
+
+    assert not _is_deb822_default(tmp_path)
+
+
+def test_is_deb822_default_wrong_format(tmp_path):
+    _is_deb822_default.cache_clear()
+    sources_file = tmp_path / "ubuntu.sources"
+    sources_file.write_text("not a sources file\n")
+
+    assert not _is_deb822_default(tmp_path)
 
 
 @pytest.mark.parametrize(
